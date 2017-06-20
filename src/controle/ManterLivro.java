0578b720af8bdaa78487abdd08cc446081748e82
Samuel Diogo <samudiogo@gmail.com>
2017-06-20 17:39:29 -0300

    #1 implementação de função para reserva de livro

--- src/controle/ManterLivro.java
+++ src/controle/ManterLivro.java
@@ -5,48 +5,73 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.LivroDao;
 import negocio.Livro;
+import negocio.Usuario;
 
 /**
  * Servlet implementation class ManterLivro
  */
 public class ManterLivro extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ManterLivro() {
-        super();
-    }
+
+	private Usuario usuario;
+	private HttpSession session;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ManterLivro() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		session = request.getSession();
+
+		usuario = (Usuario) session.getAttribute("usuario");
+		Livro livro = new Livro();
+		livro.setId(Integer.valueOf(request.getParameter("livroId")));
+		String msg = "";
+		if (usuario != null && livro.getId() > 0) {
+			if (LivroDao.ReservaLlivro(livro.getId(), usuario.getId())) {
+				msg = "Livro reservado com sucesso para " + usuario.getNome() + "!";
+			} else
+				msg = "Erro ao reservar o livro..";
+
+			request.setAttribute("msg", msg);
+			request.getRequestDispatcher("Msg.jsp").forward(request, response);
+		} else {
+			response.sendRedirect("login.html");
+		}
 
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Livro livro = new Livro(
-				Integer.valueOf(request.getParameter("id")),
-				request.getParameter("nome"),
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Livro livro = new Livro(Integer.valueOf(request.getParameter("id")), request.getParameter("nome"),
 				Integer.valueOf(request.getParameter("qtde")));
 		String msg;
-		if(LivroDao.Alterar(livro)){
+		if (LivroDao.Alterar(livro)) {
 			msg = "livro alterado com sucesso";
-		}else{
+		} else {
 			msg = "erro ao alterar o seu livro.";
 		}
-		
+
 		request.setAttribute("msg", msg);
 		request.getRequestDispatcher("Msg.jsp").forward(request, response);
-		
+
 	}
 
 }
